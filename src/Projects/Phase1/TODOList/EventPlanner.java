1ae2b75028154d79bf14948a8e5f6cc6a2ba9282
Ian Dancan Mongare <dancanian25@gmail.com>
2023-09-14 08:51:06 +0300

    Initial commit

---
+++ src/Projects/Phase1/TODOList/EventPlanner.java
@@ -0,0 +1,47 @@
+package Projects.Phase1.TODOList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventPlanner {
+
+    private List<String> events;
+//    private String EventTime;
+
+
+    public EventPlanner() {
+        events = new ArrayList<>();
+    }
+
+    public void addEvent(String eventName){
+        if (events.contains(eventName)){
+            System.out.println("Event already exists");
+            return;
+        }
+        else{
+            events.add(eventName);
+            System.out.println("Event successfully added");
+        }
+    }
+
+    public void removeEvent(String eventName){
+        if (!events.contains(eventName)){
+            System.out.println("Event does not exist");
+            return;
+        }
+        else{
+            events.remove(eventName);
+            System.out.println("Event removed");
+        }
+    }
+
+    public void listEvents(){
+
+        if (events.isEmpty()){
+            System.out.println("No activity present");
+        }
+        for (String event: events){
+            System.out.println(event);
+        }
+    }
+}
---
+++ src/Projects/Phase1/TODOList/Main.java
@@ -0,0 +1,53 @@
+package Projects.Phase1.TODOList;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        int count = 0;
+        do {
+            showOptions();
+            System.out.print("Enter choice of operation (number): ");
+            int choice = sn.nextInt();
+            sn.nextLine();
+            EventPlanner planner = new EventPlanner();
+            switch(choice){
+                   case 1 ->{
+                       System.out.println("Enter activity:");
+                       String activity = sn.nextLine();
+                       planner.addEvent(activity);
+                   }
+                   case 2-> {
+                       System.out.println("Enter activity to erase:");
+                       String activity = sn.nextLine();
+                       planner.removeEvent(activity);
+                   }
+                   case 3 -> {
+                       planner.listEvents();
+                   }
+                   case 4 -> {
+                       System.out.println("Exiting>>>>");
+                       System.exit(0);
+                   }
+                   default -> {
+                       System.out.println("Invalid choice try again");
+                   }
+            }
+
+            count++;
+        }while(count <= 5);
+
+    }
+
+    public static void showOptions(){
+        System.out.println("""
+                1. Add activity
+                2. Remove Activity
+                3. View Present Activities
+                4. Exit
+                """);
+    }
+
+}
