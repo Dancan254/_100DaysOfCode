d6b355eb0c654580f8060ff3296a0b162672fd55
Ian Dancan Mongare <dancanian25@gmail.com>
2023-09-16 22:19:14 +0300

    Adjustments using arrays

--- src/Projects/Phase1/SimpleQuizGame/Main.java
+++ src/Projects/Phase1/SimpleQuizGame/Main.java
@@ -1,5 +1,7 @@
 package Projects.Phase1.SimpleQuizGame;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,8 +9,9 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        Quiz(scanner);
 
-        QuizWithChoices(scanner);
+        //QuizWithChoices(scanner);
     }
     //with choices
     public static void QuizWithChoices(Scanner scanner){
@@ -29,6 +32,61 @@ public class Main {
             char userAnswer = scanner.next().charAt(0);
 
             if (userAnswer == answers[i]) {
+                System.out.println("Correct!\n");
+                score++;
+            } else {
+                System.out.println("Incorrect. The correct answer is " + answers[i] + ".\n");
+            }
+        }
+
+        // Display the user's final score
+        System.out.println("Quiz completed! Your score: " + score + "/" + questions.length);
+
+        // Close the scanner
+        scanner.close();
+    }
+
+    //using ArrayList
+    public static void Quiz(Scanner scanner) {
+
+        System.out.println("Welcome to Quiz game");
+
+        String[] questions = {
+                "What is Java?",
+                "Which keyword is used to define a constant in Java?",
+                "What is the entry point of a Java program?",
+                "Which data type is used to store whole numbers in Java?",
+                "How do you declare a variable in Java to hold a floating-point number?",
+                "What is the purpose of the 'if' statement in Java?",
+                "Which Java keyword is used to create a subclass of a class?",
+                "What is the default value of a boolean variable in Java?",
+                "Which loop in Java is used to iterate over a collection of items like an array?",
+                "How do you prevent a method or variable from being accessed outside of its class?"
+        };
+
+        String[] answers = {
+                "B) A programming language",
+                "B) final",
+                "A) main()",
+                "B) int",
+                "C) double",
+                "C) To make decisions in code",
+                "B) extends",
+                "B) false",
+                "B) for-each",
+                "B) Make it private"
+        };
+
+
+        int score = 0;
+        for (int i = 0; i < questions.length; i++) {
+            System.out.println("question" + (i + 1) + ": " + questions[i]);
+            System.out.println("A) An operating system");
+            System.out.println(answers[i]);
+
+            char userAnswer = scanner.next().charAt(0);
+
+            if (userAnswer == answers[i].charAt(0)) {
                 System.out.println("Correct!\n");
                 score++;
             } else {
