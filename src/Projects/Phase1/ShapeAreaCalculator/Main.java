88bcf2a4a0c6efbf8623abc58e6ebf0228e0aec9
Ian Dancan Mongare <dancanian25@gmail.com>
2023-09-17 21:53:29 +0300

    Implementing Scanner

--- src/Projects/Phase1/ShapeAreaCalculator/Main.java
+++ src/Projects/Phase1/ShapeAreaCalculator/Main.java
@@ -1,12 +1,36 @@
 package Projects.Phase1.ShapeAreaCalculator;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Shapes<Triangle> triangle = new Shapes<>(new Triangle(10, 5, 7));
-        Shapes<Circle> circle = new Shapes<>(new Circle(10));
-        Shapes<Rectangle> rectangle = new Shapes<>(new Rectangle(10,  7));
+//        Shapes<Triangle> triangle = new Shapes<>(new Triangle(10, 5, 7));
+//        Shapes<Circle> circle = new Shapes<>(new Circle(10));
+//        Shapes<Rectangle> rectangle = new Shapes<>(new Rectangle(10,  7));
+
+        //implementing scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Create records for different shapes with user-provided input
+        System.out.print("Enter the radius of the circle: ");
+        double circleRadius = scanner.nextDouble();
+        Shapes<Circle> circle = new Shapes<>(new Circle(circleRadius));
+
+        System.out.print("Enter the length of the rectangle: ");
+        double rectangleLength = scanner.nextDouble();
+        System.out.print("Enter the width of the rectangle: ");
+        double rectangleWidth = scanner.nextDouble();
+        Shapes<Rectangle> rectangle = new Shapes<>(new Rectangle(rectangleLength, rectangleWidth));
+
+        System.out.print("Enter the base of the triangle: ");
+        double triangleBase = scanner.nextDouble();
+        System.out.print("Enter the height of the triangle: ");
+        double triangleHeight = scanner.nextDouble();
+        System.out.print("Enter the first side of the triangle: ");
+        double triangleHypotenus = scanner.nextDouble();
+        Shapes<Triangle> triangle = new Shapes<>(new Triangle(triangleBase,triangleHeight, triangleHypotenus));
 
         System.out.println("Areas of the shapes");
         System.out.println("Triangle area = " + triangle.Area());
