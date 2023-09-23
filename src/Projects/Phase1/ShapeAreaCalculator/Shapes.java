e599160fdedea923cd82f0668fe668fcc5c9fd33
Ian Dancan Mongare <dancanian25@gmail.com>
2023-09-23 22:31:14 +0300

    Inline comment

--- src/Projects/Phase1/ShapeAreaCalculator/Shapes.java
+++ src/Projects/Phase1/ShapeAreaCalculator/Shapes.java
@@ -1,36 +1,41 @@
 package Projects.Phase1.ShapeAreaCalculator;
+// Define record classes for Rectangle, Circle, and Triangle
+record Rectangle(double length, double width) {}
 
+record Circle(double radius) {}
 
-record Rectangle(double length, double width){}
-record Circle(double radius){}
-record Triangle(double base, double height, double hypotenuse){}
-public class Shapes <T> {
+record Triangle(double base, double height, double hypotenuse) {}
 
-   private T shapeType;
+// Shapes class with generic type T
+public class Shapes<T> {
 
+    private T shapeType;
+
+    // Constructor that initializes the shapeType
     public Shapes(T shapeType) {
         this.shapeType = shapeType;
     }
 
+    // Getter method for shapeType
     public T getShapeType() {
         return shapeType;
     }
 
-    //method to calculate area, returns double
-    public double Area(){
-        if (shapeType instanceof Rectangle rectangle){
+    // Method to calculate the area of different shapes
+    public double Area() {
+        if (shapeType instanceof Rectangle rectangle) {
             return rectangle.length() * rectangle.width();
         } else if (shapeType instanceof Circle circle) {
             return Math.PI * Math.pow(circle.radius(), 2);
-        }
-        else if (shapeType instanceof Triangle triangle){
+        } else if (shapeType instanceof Triangle triangle) {
             return 0.5 * triangle.base() * triangle.height();
-        }
-        else{
+        } else {
+            // Default case when shapeType is unknown
             return 0.0;
         }
     }
 
+    // Method to calculate the perimeter of different shapes
     public double calculatePerimeter() {
         if (shapeType instanceof Circle circle) {
             return 2 * Math.PI * circle.radius();
@@ -38,7 +43,9 @@ public class Shapes <T> {
             return 2 * (rectangle.length() + rectangle.width());
         } else if (shapeType instanceof Triangle triangle) {
             return triangle.base() + triangle.height() + triangle.hypotenuse();
+        } else {
+            // Default case when shapeType is unknown
+            return 0.0;
         }
-        return 0.0;
     }
 }
